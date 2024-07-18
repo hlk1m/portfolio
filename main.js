12821dc667f974c18645be7550a91c4190139564
hlk1m <gywjd5126@naver.com>
2024-07-18 22:11:56 +0900

    JS

---
+++ main.js
@@ -0,0 +1,167 @@
+"use strict";
+import {
+  onScrollToElement,
+  onWorkCardHover,
+  makeWorkCardContent,
+  makeDetailPage,
+} from "./utils.js";
+
+let fetchData = [];
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Work 데이터 가져오기 및 출력
+  fetch("./data/data.json")
+    .then((res) => res.json())
+    .then((json) => {
+      fetchData = json.project;
+      printWorkCard(fetchData);
+    })
+    .catch((error) => console.log(error));
+
+  // 화면에 요소가 나타나면 애니메이션을 실행함.
+  const elements = document.querySelectorAll('[data-animate="true"]');
+
+  const observer = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("animation");
+          observer.unobserve(entry.target); // Stop observing once animated
+        }
+      });
+    },
+    {
+      threshold: 0.5, // Adjust this value as needed
+    }
+  );
+
+  elements.forEach((element) => {
+    observer.observe(element);
+  });
+});
+
+// Scroll Event
+const home = document.querySelector("#home");
+const homeHeight = home.getBoundingClientRect().height;
+
+const goTopBtn = document.querySelector(".go_top_btn");
+const navbar = document.querySelector("#navbar");
+
+document.addEventListener("scroll", () => {
+  if (window.scrollY > homeHeight) {
+    navbar.classList.add("animation");
+  } else if (window.scrollY > homeHeight / 2) {
+    navbar.classList.remove("animation");
+    goTopBtn.classList.remove("hidden");
+  } else {
+    goTopBtn.classList.add("hidden");
+  }
+
+  // control home opacity - slowly fade out
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+// Navbar menu scroll event
+document.querySelector(".navbar_menu").addEventListener("click", (e) => {
+  const link = e.target.dataset.link;
+  if (!link) return;
+
+  onScrollToElement(link);
+});
+
+// Navbar toggle button
+document
+  .querySelector(".navbar_menu_btn")
+  .addEventListener("click", () => navbarMenu.classList.toggle("hidden"));
+
+// Go to top button
+goTopBtn.addEventListener("click", () => onScrollToElement("#home"));
+
+// Work card Buttons
+let num = 0;
+
+function onSlideAnimation(isRight) {
+  const workCard = document.querySelector(".work_card");
+  if (isRight) {
+    if (num >= 4) return;
+
+    num++;
+    workCard.classList.add("right-animation");
+  } else {
+    if (num <= 0) return;
+
+    num--;
+    workCard.classList.add("left-animation");
+  }
+
+  printWorkCard(fetchData);
+
+  setTimeout(() => {
+    workCard.className = "work_card";
+  }, 1000);
+}
+
+document
+  .querySelector(".right_btn")
+  .addEventListener("click", () => onSlideAnimation(true));
+
+document
+  .querySelector(".left_btn")
+  .addEventListener("click", () => onSlideAnimation(false));
+
+// Print work card
+function printWorkCard(data) {
+  const workCard = document.querySelector(".work_card");
+  const detailPage = document.querySelector(".detail");
+
+  workCard.innerHTML = makeWorkCardContent(data[num]);
+  detailPage.innerHTML = makeDetailPage(data[num]);
+
+  // Toggle More button
+  const cardMoreBtn = document.querySelector(".card_more");
+
+  cardMoreBtn.addEventListener("click", () => {
+    const description = document.querySelector(".card_description");
+
+    if (description.classList.contains("hidden")) {
+      description.classList.remove("hidden");
+      cardMoreBtn.innerHTML = `상세 설명 접기 <i class="fa-solid fa-chevron-up"></i>`;
+    } else {
+      description.classList.add("hidden");
+      cardMoreBtn.innerHTML = `상세 설명 더보기 <i class="fa-solid fa-chevron-down"></i>`;
+    }
+  });
+
+  const mainImg = document.querySelector(".card_main");
+
+  // Work Main img - Open Detail page
+  mainImg.addEventListener("click", () =>
+    detailPage.classList.toggle("hidden")
+  );
+
+  // Work Main image - Hover Event
+  mainImg.addEventListener("mouseover", (e) => {
+    const pageX = e.pageX;
+    const width = window.innerWidth;
+
+    const left = width * 0.15;
+    const right = width * 0.6;
+
+    // Main image가 아닌 곳에서 hover event를 적용시키지 않음.
+    if (pageX < left || pageX > right) {
+      return;
+    }
+
+    onWorkCardHover(true);
+  });
+
+  mainImg.addEventListener("mouseleave", () => onWorkCardHover(false));
+
+  // Close Detail page
+  document.querySelector(".detail_close_btn").addEventListener("click", () => {
+    detailPage.classList.toggle("hidden");
+    onWorkCardHover(false);
+  });
+
+  onWorkCardHover(false);
+}
---
+++ utils.js
@@ -0,0 +1,132 @@
+"use strict";
+
+// Scroll to section
+function onScrollToElement(id) {
+  document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+}
+
+// Main image hover event - Toggle Detail page
+let detailTimeout;
+function onWorkCardHover(isHover) {
+  const loadingIcon = document.querySelector(".loading_icon");
+  const cardImg = document.querySelector(".card_img");
+  const detailPage = document.querySelector(".detail");
+
+  if (!isHover) {
+    clearTimeout(detailTimeout);
+    loadingIcon.classList.add("hidden");
+    cardImg.classList.remove("hover");
+    return;
+  }
+
+  cardImg.classList.add("hover");
+  loadingIcon.classList.remove("hidden");
+
+  detailTimeout = setTimeout(() => {
+    detailPage.classList.remove("hidden");
+  }, 2200);
+}
+
+// Make Work, Detail Content
+function makeWorkCardContent(data) {
+  const description = data.description;
+  return `
+        <figure class="card_main">
+            <img
+              src="assets/works/mockup/${data.type}_m.jpg"
+              alt="work thumbnail"
+              class="card_img"
+            />
+            <div class="click_btn">click! <i class="fa-solid fa-arrow-up"></i></div>
+          </figure>
+          <div class="card_content">
+            <h5>${data.title}</h5>
+            <div class="card_list">
+              <span>Tool</span>
+              <p>${data.languages}</p>
+            </div>
+            <div class="card_list">
+              <span>참여도</span>
+              <p>개인 프로젝트</p>
+            </div>
+            <div class="card_list">
+              <span>주요 기능</span>
+              <p>${data.feature}</p>
+            </div>
+            <div class="card_links">
+              <a href="${
+                data.url
+              }" target="_blank" class="card_link_btn">웹사이트</a>
+              <a href="${
+                data.github
+              }" target="_blank" class="card_link_btn">Github</a>
+            <button class="card_link_btn card_detail_btn hidden">자세히 보기</button>
+            </div>
+            <button class="card_more">
+              상세 설명 더보기 <i class="fa-solid fa-chevron-down"></i>
+            </button>
+            <p class="card_description hidden">
+            ${
+              description.length > 150
+                ? description.slice(0, 150) + "..."
+                : description
+            }
+            </p>
+          </div>         
+`;
+}
+
+function makeDetailPage(data) {
+  const image = data.imageNum;
+  const imageArray = Array(image).fill(0); // make array - length, to fill
+
+  return `
+        <div class="detail_heading">
+          <h1 class="detail_title">${data.title}</h1>
+          <button class="detail_close_btn">
+            <i class="fa-solid fa-xmark"></i>
+          </button>
+        </div>
+        <ul class="detail_content">
+          <li class="detail_list">
+            <span>Tool</span>
+            <p>${data.languages}</p>
+          </li>
+          <li class="detail_list">
+            <span>참여도</span>
+            <p>개인 프로젝트</p>
+          </li>
+          <li class="detail_list">
+            <span>주요 기능</span>
+            <p>${data.feature}</p>
+          </li>
+          <li class="detail_list">
+            <span>Links</span>
+            <a href="${
+              data.url
+            }" target="_blank" class="card_link_btn">웹사이트</a>
+            <a href="${
+              data.github
+            }" target="_blank" class="card_link_btn">Github</a>
+          </li>
+        </ul>
+        <div class="detail_description">
+          <h2 class="detail_title">Description</h2>
+          <p>${data.description}</p>
+        </div>
+        <figure class="detail_img">
+          <h2 class="detail_title">details</h2>
+          ${imageArray.map(
+            (_, index) =>
+              `<img src="assets/works/${data.type}_${index + 1}.jpg" />`
+          )}
+        </figure>
+  `;
+}
+
+export {
+  onWorkCardHover,
+  onScrollToElement,
+  makeWorkCardContent,
+  makeDetailPage,
+};
